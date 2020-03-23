81181923b6af66c15514c8090d863eff0c7bd0a8
pattanunNP <phattanun@hotmail.com>
2020-03-24 04:34:12 +0700

    bulid_page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Home from "./screen/Home";
+import Mac from "./screen/Mac";
+import Buy from "./screen/Buy";
+import support from "./screen/support";
+import swift7 from "./screen/swift7";
+import surface from "./screen/surface";
+import XPS from "./screen/xps";
+import Bag from "./screen/bag";
+import Zenbook from "./screen/zenbook";
+import Notfound from "./screen/notfound";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/support" component={support} />
+        <Route exact path="/product/macbook-pro-13" component={Mac} />
+        <Route exact path="/product/swift-7" component={swift7} />
+        <Route exact path="/product/surface-pro-x" component={surface} />
+        <Route exact path="/product/dell-xps" component={XPS} />
+        <Route exact path="/product/zenbook" component={Zenbook} />
+        <Route exact path="/shop/bag" component={Bag} />
+        <Route exact path="/buy/id=:id" component={Buy} />
+        <Route exzvt path="*" component={Notfound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
